4b65df7d31676581be22286fc55485e5a47522bd
StickCode <manuel@netixia.fr>
2013-07-17 23:43:05 +0200

    Initial commi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StickCodeApp::Application.config.secret_key_base = 'ffa378a38412a4932a5b58e1ecac3c3762a942c00e9c74dce3811f9799eca4a53d7c076953040f3f03e4e9cdcff0bf37d4cf2d2d60a762b0088ae90d2ea954e1'
